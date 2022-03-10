4ad5d8cb116fdec0ea7ba2b96d204a4f275c3e7f
vuongbaoremix <vuongbaoremix@gmail.com>
2022-03-11 01:04:47 +0700

    update regex get image

--- NextVozDownloadImage/Helpers/NextVozRegex.cs
+++ NextVozDownloadImage/Helpers/NextVozRegex.cs
@@ -13,7 +13,7 @@ namespace NextVozDownloadImage.Helpers
         private const string REGEX_THREAD_NAME = @"<meta property=""og:title"" content=""(.*)"" \/>";
         private const string REGEX_PAGE = @"pageNav-page.*\/page-(.*)"">.*[^<]*<\/ul>";
         private const string REGEX_ATTACHMENT = @"<a.* href=""\/attachments\/(.*)\/"" target=""_blank""";
-        private const string REGEX_IMAGE = @"class=""bbImageWrapper.*\n.*data-src=""([^""]+)""";
+        private const string REGEX_IMAGE = @"class=""bbImageWrapper.*data-src=""([^""]+)""";
         private const string REGEX_LINK_EXTERNAL = @"<a href=.* class=""link link--external"".*\n.*\n.*<img src=""([^""]+)""";
         private const string REGEX_THREAD = @"voz.vn\/t\/([^\/]*)";
         private const string REGEX_Get_PAGE_FROM_URL = @"\/page-(.*)";
